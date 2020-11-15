e48ac9b6561f4f2afe7c13b63e31708fc5fea793
mkiryaha <73002846+mkiryaha@users.noreply.github.com>
2020-11-15 13:54:12 +0300

    Create ru.js

---
+++ src/translations/ru.js
@@ -0,0 +1,38 @@
+{
+  "common": {
+    "name": "Очиститель воздуха",
+    "description": "Карточка позволяет управлять вашим очистителем воздуха.",
+    "not_available": "Сущность недоступна",
+    "toggle_power": "Включить/Выключить"
+  },
+  "state": {
+    "on": "Включён",
+    "off": "Выключен"
+  },
+  "speed": {
+    "Auto": "Авто",
+    "Silent": "Тихий",
+    "Favorite": "Избранный",
+    "Fan": "Вентилятор"
+  },
+  "error": {
+    "missing_entity": "Необходимо указать сущность!",
+    "xiaomi_miio_level_without_speed": "Необходимо указать поле speed вместе с xiaomi_miio_favorite_level!"
+  },
+  "editor": {
+    "entity": "Сущность (обязательно)",
+    "compact_view": "Компактный вид",
+    "compact_view_aria_label_on": "Включить компактный вид",
+    "compact_view_aria_label_off": "Выключить компактный вид",
+    "show_name": "Показывать имя?",
+    "show_name_aria_label_on": "Показывать имя",
+    "show_name_aria_label_off": "Скрыть имя",
+    "show_state": "Показывать статусы?",
+    "show_state_aria_label_on": "Показывать статусы",
+    "show_state_aria_label_off": "Скрыть статусы",
+    "show_toolbar": "Показывать панель инструментов?",
+    "show_toolbar_aria_label_on": "Показать панель инструментов",
+    "show_toolbar_aria_label_off": "Скрыть панель инструментов",
+    "code_only_note": "Внимание: Настройка статусов и действий возможна только через редактор кода."
+  }
+}
