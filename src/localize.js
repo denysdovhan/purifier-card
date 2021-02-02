7266566db610b794147ac7aa67fb3807597348ef
Eric Reiche <e@reiche.me>
2021-02-02 17:28:25 +0100

    Add de/German to localize.js

--- src/localize.js
+++ src/localize.js
@@ -8,6 +8,7 @@ import * as fr from './translations/fr.json';
 import * as nb from './translations/nb.json';
 import * as pl from './translations/pl.json';
 import * as bg from './translations/bg.json';
+import * as de from './translations/de.json';
 
 var languages = {
   en,
@@ -17,6 +18,7 @@ var languages = {
   nb,
   pl,
   bg,
+  de,
 };
 
 const DEFAULT_LANG = 'en';
